f89bbff8f8e348ef613ad127153f1324d221e88f
angarosxx <ag@corrporis.net>
2026-06-18 13:26:49 +0200

    Update login to error logs

--- web/public/api/auth/login_process.php
+++ web/public/api/auth/login_process.php
@@ -10,8 +10,9 @@ error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '0');
 
 // Absolute safety net against structural warnings leaking into response streams
-error_reporting(E_ERROR | E_PARSE);
-ini_set('display_errors', '0');
+//error_reporting(E_ERROR | E_PARSE);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
