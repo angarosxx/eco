4e0130fddcb05b6e968e8a94469e2a3f1e238a13
angarosxx <ag@corrporis.net>
2026-06-25 08:56:35 +0200

    Update package.json

---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Views/**/*.php",
+    "./public/**/*.php",
+    "./index.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
